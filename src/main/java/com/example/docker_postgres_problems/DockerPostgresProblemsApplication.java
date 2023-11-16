ba300655f13b2df21a09bdf90c1351931f63abc0
lobashev-andrey <lobashev.andrey@mail.ru>
2023-11-16 12:34:14 +0300

    Initial commit

---
+++ src/main/java/com/example/docker_postgres_problems/DockerPostgresProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker_postgres_problems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPostgresProblemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerPostgresProblemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/docker_postgres_problems/DockerPostgresProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker_postgres_problems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerPostgresProblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
